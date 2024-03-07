48237c8faf4d557251dc53581b8027d7dc9a5900
sodex4real11 <sodex4real11@gmail.com>
2024-03-07 15:29:26 -0800

    api advanced~

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data").get("subscribers")
-    return results.get("subscribers")
+    
+    if response.status_code != 200:
+        print("Error: Unable to fetch data from Reddit API")
+        return None
+
+    data = response.json().get("data")
+    if not data:
+        print("No data found for the subreddit: {}".format(subreddit))
+        return None
+
+    subscribers = data.get("subscribers")
+    if subscribers is None:
+        print("No subscriber information found for the subreddit: {}".format(subreddit))
+        return None
+
+    return subscribers
